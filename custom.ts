9c0de9c0a174d0dfe8375320b54d76e8838eadd2
jomihal <40837398+jomihal@users.noreply.github.com>
2023-11-19 18:08:24 +0200

    Ενημέρωση pxt.json, main.blocks, main.ts, custom.ts

--- custom.ts
+++ custom.ts
@@ -4,67 +4,96 @@
 * Διαβάστε περισσότερα στο https://makecode.microbit.org/blocks/custom
 */
 
-enum MyEnum {
-    //% block="one"
-    One,
-    //% block="two"
-    Two
-}
 enum GigoMotorPort {
-    'E',
-    'F',
-    'G',
-    'H'
-    
+    E,
+    F,
+    G,
+    H
 }
+
+enum LedPort {
+    B, C, D, E, F, G, H
+    }
+
 /**
  * myGigo blocks
  */
 //% weight=100 color=#0fbc11 icon=""
 namespace myGigo {
     /**
-     * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
+     * Κινητήρας: Έλεγχος κινητήρα
+     * Ταχύτητα Speed : ταχύτητα περιστροφής 0-255
+     * Κατεύθυνση Direction: φορά περιστροφής 0 ή 1 για αριστερόστροφα ή δεξιόστροφα
+     * Θύρα Port : Θύρα σύνδεσης του κινητήρα: E, F, G, H
      */
-    //% block
-    export function foo(n: number, s: string, e: MyEnum): void {
-        // Add code here
-    }
-
-    /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
-     */
-    //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value - 1) + fib(value - 2);
-    }
-
-    //% block="Κινητήρας σε θύρα $Port με κατεύθυνση $Direction και ταχύτητα $Speed"
+    
+    //% block="Κινητήρας σε ταχύτητα $Speed με κατεύθυνση $Direction στη θύρα $Port"
     //% Direction.min=0 Direction.max=1
     //% Speed.min=0 Speed.max=255
-    //% Port.min=1 Port.max=4
     export function Motor(Speed: number, Direction: number, Port: GigoMotorPort) {
-        switch(Port) {
-            case GigoMotorPort.E:
-                {   pins.analogWritePin(AnalogPin.P15, pins.map(Direction, 0, 1, 0, 1023));
-                    pins.analogWritePin(AnalogPin.P16, pins.map(Speed, 0, 255, 0, 1023));
-                }
-            case GigoMotorPort.F: {
+        if (Port == GigoMotorPort.E) {
+            pins.analogWritePin(AnalogPin.P15, pins.map(Direction, 0, 1, 0, 1023));
+            pins.analogWritePin(AnalogPin.P16, pins.map(Speed, 0, 255, 0, 1023));
+        } else {
+            if (Port == GigoMotorPort.F) {
                 pins.analogWritePin(AnalogPin.P13, pins.map(Direction, 0, 1, 0, 1023));
                 pins.analogWritePin(AnalogPin.P14, pins.map(Speed, 0, 255, 0, 1023));
+            } else {
+                if (Port == GigoMotorPort.G) {
+                    pins.analogWritePin(AnalogPin.P12, pins.map(Direction, 0, 1, 0, 1023));
+                    pins.analogWritePin(AnalogPin.P2, pins.map(Speed, 0, 255, 0, 1023));
+                } else {
+                    if (Port == GigoMotorPort.H) {
+                        pins.analogWritePin(AnalogPin.P1, pins.map(Direction, 0, 1, 0, 1023));
+                        pins.analogWritePin(AnalogPin.P8, pins.map(Speed, 0, 255, 0, 1023));
+                    }
                 }
-            case GigoMotorPort.G: {
-                pins.analogWritePin(AnalogPin.P12, pins.map(Direction, 0, 1, 0, 1023));
-                pins.analogWritePin(AnalogPin.P2, pins.map(Speed, 0, 255, 0, 1023));
             }
-            case GigoMotorPort.H: {
-                pins.analogWritePin(AnalogPin.P1, pins.map(Direction, 0, 1, 0, 1023));
-                pins.analogWritePin(AnalogPin.P8, pins.map(Speed, 0, 255, 0, 1023));
-            }            
         }
 
+    }
+
+
+    /**
+     * GigoLed: Έλεγχος εξωτερικού Led
+     * Κατάσταση Status : αναμμένο ή σβηστό 0 ή 1
+     * Θύρα Port : Θύρα σύνδεσης του Led: B, C, D, E, F, G, H
+     */
+
+    //% block="Led σε κατάσταση $Status στη θύρα $Port"
+    //% Status.min=0 Status.max=1
+    export function GigoLed(Status: number, Port: LedPort) {
+        if (Port == LedPort.B) {
+            pins.digitalWritePin(DigitalPin.P14, Status);
+        }
+        else {
+            if (Port == LedPort.C) {
+                pins.digitalWritePin(DigitalPin.P2, Status);
+            }
+            else {
+                if (Port == LedPort.D) {
+                    pins.digitalWritePin(DigitalPin.P8, Status);
+                } else {
+                    if (Port == LedPort.E) {
+                        pins.digitalWritePin(DigitalPin.P15, Status);
+                    }
+                    else {
+                        if (Port == LedPort.F) {
+                            pins.digitalWritePin(DigitalPin.P13, Status);
+                        }
+                        else {
+                            if (Port == LedPort.G) {
+                                pins.digitalWritePin(DigitalPin.P12, Status);
+                            }
+                            else {
+                                if (Port == LedPort.H) {
+                                    pins.digitalWritePin(DigitalPin.P1, Status);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
     }
 }
