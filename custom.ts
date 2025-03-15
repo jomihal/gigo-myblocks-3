d7899dcebeeec8e5be7a9c2a5ae5bd8e46bc5ada
jomihal <40837398+jomihal@users.noreply.github.com>
2025-03-15 09:38:32 +0200

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -33,12 +33,8 @@ namespace myGigo {
     //% Speed.min=0 Speed.max=255
     export function Motor(Speed: number, Direction: number, Port: GigoMotorPort) {
         if (Port == GigoMotorPort.E) {
-            if (Direction == 0) {
-                pins.analogWritePin(AnalogPin.P15,0);
-                pins.analogWritePin(AnalogPin.P16, pins.map(Speed, 0, 255, 0, 1023));}
-            else {
-                pins.analogWritePin(AnalogPin.P16,0);
-                pins.analogWritePin(AnalogPin.P15, pins.map(Speed, 0,255,0,1023));}
+                pins.analogWritePin(AnalogPin.P15, pins.map(Direction, 0, 1, 0, 1023));
+                pins.analogWritePin(AnalogPin.P16, pins.map(Speed, 0, 255, 0, 1023)); 
         } else {
             if (Port == GigoMotorPort.F) {
                 pins.analogWritePin(AnalogPin.P13, pins.map(Direction, 0, 1, 0, 1023));
